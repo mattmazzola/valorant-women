a63dce2a6dfff70f6ad5516ad9b59741503f205e
Matt Mazzola <mattmazzola@live.com>
2022-07-24 15:33:36 -0700

    Fix container auto start

--- sql/entrypoint.sh
+++ sql/entrypoint.sh
@@ -1,1 +1,2 @@
-/opt/mssql/bin/sqlserver & ./setup.sh & sleep infinity & wait
+#start SQL Server, start the script to create the DB and import the data, start the app
+/opt/mssql/bin/sqlservr & ./setup.sh & sleep infinity & wait
--- sql/setup.sh
+++ sql/setup.sh
@@ -1,3 +1,3 @@
-./wait-for-it.sh sql:1433 --timeout=0 --strict -- sleep 5s && echo "$SA_PASSWORD" && \
-/opt/mssql-tools/bin/sqlcmd -S localhost -i InitializeDatabase.sql -U SA -P "<YourStrong@Passw0rd>"
-# /opt/mssql-tools/bin/sqlcmd -S localhost -i InitializeDatabase.sql -U SA -P "$SA_PASSWORD"
+# Wait for SQL Server to be started and then run the sql script
+./wait-for-it.sh sql:1433 --timeout=0 --strict -- sleep 5s && \
+/opt/mssql-tools/bin/sqlcmd -S localhost -i InitializeDatabase.sql -U SA -P "$SA_PASSWORD"
