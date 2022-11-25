aa6f3b070d7495f272a2a3e5bba2557c25ba2b67
Matt Mazzola <mattmazzola@fastmail.com>
2022-11-24 17:50:49 -0800

    Add types to service

--- client-remix/app/service/ratingsService.ts
+++ client-remix/app/service/ratingsService.ts
@@ -1,17 +1,17 @@
 import invariant from "tiny-invariant"
-import { Submission } from "~/models"
+import { SavedSubmission, Submission } from "~/models"
 
 invariant(typeof process.env.API_URL === 'string', `env.API_URL must be a non-empty string`)
 const apiBaseUrl = process.env.API_URL
 
-export async function getRatings(sex: "men" | "women") {
+export async function getRatings(sex: "men" | "women"): Promise<SavedSubmission[]> {
     const request = await fetch(`${apiBaseUrl}/ratings?gender=${sex}`)
     const json = await request.json()
 
     return json
 }
 
-export async function postRating(rating: Submission) {
+export async function postRating(rating: Submission): Promise<SavedSubmission> {
     const request = await fetch(`${apiBaseUrl}/ratings`, {
         method: 'POST',
         headers: {
