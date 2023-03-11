05ab9d5ebf7bb03c4fd12946c94230ee80c2141a
Matt Mazzola <mattmazzola@live.com>
2023-03-11 14:10:46 -0800

    Extract urlSearchParamsString

--- client-remix/app/routes/index.tsx
+++ client-remix/app/routes/index.tsx
@@ -2,6 +2,9 @@ import { DataFunctionArgs, redirect } from "@remix-run/node"
 
 export const loader = async ({ request }: DataFunctionArgs) => {
   const url = new URL(request.url)
+  const urlSearchParamsString = [...url.searchParams.entries()].length > 0
+    ? `?${url.searchParams}`
+    : ''
 
-  return redirect(`ratings${[...url.searchParams.entries()].length > 0 ? `?${url.searchParams}` : ''}`)
+  return redirect(`ratings${urlSearchParamsString}`)
 }
--- client-remix/app/routes/ratings/index.tsx
+++ client-remix/app/routes/ratings/index.tsx
@@ -5,14 +5,15 @@ export const loader: LoaderFunction = async ({ request }) => {
     const url = new URL(request.url)
     // TODO: Is there browser utility for URL manipulation that resolves double slashes?
     const baseUrl = url.pathname.endsWith('/')
-        ? url.pathname.slice(0,-1)
+        ? url.pathname.slice(0, -1)
         : url.pathname
-        
+
     const activeSex = await getActiveSex(request)
-    
-    const childRoute = `${baseUrl}/${activeSex.toLowerCase()}${[...url.searchParams.entries()].length > 0
+    const urlSearchParamsString = [...url.searchParams.entries()].length > 0
         ? `?${url.searchParams}`
-        : ''}`
+        : ''
+
+    const childRoute = `${baseUrl}/${activeSex.toLowerCase()}${urlSearchParamsString}`
 
     return redirect(childRoute)
 }
