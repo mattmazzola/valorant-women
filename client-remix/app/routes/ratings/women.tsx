492fb05f8f4ee2b7c388b7e27eb3c68d4116b522
Matt Mazzola <mattmazzola@live.com>
2023-06-30 21:40:58 -0700

    Attempt to switch to Clerk Auth

--- client-remix/app/routes/ratings/women.tsx
+++ client-remix/app/routes/ratings/women.tsx
@@ -1,68 +1,73 @@
-import { DataFunctionArgs, json } from "@remix-run/node"
+import { useUser } from "@clerk/remix"
+import { createClerkClient } from "@clerk/remix/api.server"
+import { getAuth } from "@clerk/remix/ssr.server"
+import { LoaderArgs, json } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react"
 import StaticRating from '~/components/StaticRating'
 import StaticRatingsList from '~/components/StaticRatingsList'
 import { femaleAgents } from '~/constants'
 import { getAgentNamesSortedByRating } from "~/helpers"
-import { auth, commitSession, getSession } from "~/services/auth.server"
-import { managementClient } from "~/services/auth0management.server"
+import { commitSession, getSession } from "~/services/auth.server"
 import { getRatings } from "~/services/ratingsService"
 import { hasUserSubmittedFemaleRatingKey } from "../ratings"
 
-export const loader = async ({ request }: DataFunctionArgs) => {
-    const users = await managementClient.getUsers()
-    const submissions = await getRatings("women")
-    for (const submission of submissions) {
-        const user = users.find(u => u.user_id === submission.userId)
-        if (user) {
-            submission.user = user
-        }
+export const loader = async (args: LoaderArgs) => {
+  const { userId } = await getAuth(args)
+  const clerkClient = createClerkClient({
+    secretKey: process.env.CLERK_SECRET_KEY
+  })
+  const users = await clerkClient.users.getUserList()
+  const submissions = await getRatings("women")
+  for (const submission of submissions) {
+    const user = users.find(u => u.id === submission.userId)
+    if (user) {
+      submission.user = user
     }
+  }
 
-    const profile = await auth.isAuthenticated(request)
-    const session = await getSession(request.headers.get("Cookie"))
-    
-    let hasUserSubmittedRating = true
-    if (profile) {
-        hasUserSubmittedRating = Boolean(submissions.find(s => s.userId === profile.id))
-        session.set(hasUserSubmittedFemaleRatingKey, hasUserSubmittedRating)
+  const session = await getSession(args.request.headers.get("Cookie"))
+
+  let hasUserSubmittedRating = true
+  if (userId) {
+    hasUserSubmittedRating = Boolean(submissions.find(s => s.userId === userId))
+    session.set(hasUserSubmittedFemaleRatingKey, hasUserSubmittedRating)
+  }
+
+  return json({
+    submissions,
+    hasUserSubmittedRating,
+  }, {
+    headers: {
+      "Set-Cookie": await commitSession(session),
     }
-
-    return json({
-        profile,
-        submissions,
-        hasUserSubmittedRating,
-    }, {
-        headers: {
-            "Set-Cookie": await commitSession(session),
-        }
-    })
+  })
 }
 
 export default function RatingWomen() {
-    const { profile, submissions } = useLoaderData<typeof loader>()
-    const avgWomenRatingNames = getAgentNamesSortedByRating(submissions, femaleAgents)
+  const { submissions } = useLoaderData<typeof loader>()
+  const { user } = useUser()
+  const avgWomenRatingNames = getAgentNamesSortedByRating(submissions, femaleAgents)
 
-    return (
-        <>
-            <section>
-                <h2>What the People Think:</h2>
-                <p>Based on the average of all the {submissions.length} ratings this is what the people think.</p>
-                <StaticRating
-                    sortedAgentNames={avgWomenRatingNames}
-                    agents={femaleAgents}
-                />
-            </section>
+  return (
+    <>
+      <section>
+        <h2>What the People Think:</h2>
+        <p>Based on the average of all the {submissions.length} ratings this is what the people think.</p>
+        <StaticRating
+          sortedAgentNames={avgWomenRatingNames}
+          agents={femaleAgents}
+        />
+      </section>
 
-            <section>
-                <h2>Individual Ratings ({submissions.length})</h2>
-                <p>Ratings by individual submissions.</p>
-                <StaticRatingsList
-                    currentUserId={profile?.id}
-                    submissions={submissions}
-                    agents={femaleAgents}
-                />
-            </section>
-        </>
-    )
+      <section>
+        <h2>Individual Ratings ({submissions.length})</h2>
+        <p>Ratings by individual submissions.</p>
+        <StaticRatingsList
+          currentUserId={user?.id}
+          submissions={submissions as any[]}
+          agents={femaleAgents}
+        />
+      </section>
+    </>
+  )
 }
